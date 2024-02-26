cd4ff0c2fb33f188a80bbf2a785f7efbdcf4eb84
rick xuoxod <xuoxod@gmail.com>
2024-02-26 12:58:52 -0500

    Added script

---
+++ constants.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+declare -r PATH_PATTERN='^((/)?([a-zA-Z]+)(/[a-zA-Z]+/?)?$|/)'
+declare -r WORD_PATTERN='^[a-zA-Z]+$'
+declare -r NUMBER_PATTERN='^[0-9]+$'
+declare -r ALPHA_NUM_PATTERN='^([0-9]+|[A-Za-z]+)$'
